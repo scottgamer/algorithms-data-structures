0a29b0cbb145424b922cc255eeaecb6c9a1c9056
Richard Muñoz <richard.munoz@crifa.com>
2019-10-31 11:02:34 -0500

    added capitalize solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,13 +8,27 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  const words =[];
+  const words = [];
 
-for(let word of str.split(" ")){
-  words.push(word[0].toUpperCas);
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(" ");
 }
 
-  
-}
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === " ") {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+
+//   return result;
+// }
 
 module.exports = capitalize;
